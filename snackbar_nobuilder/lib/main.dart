22c41ac33b950d47f0f8e9646f3cbf4f54427db2
jiwoo Yun <riudiu@pood.pet>
2021-12-16 14:37:31 +0900

    Builder 위젯 없이 스낵바 만들기

---
+++ snackbar_nobuilder/lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'snack Bar',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: MyPage(),
+    );
+  }
+}
+
+class MyPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Snack Bar'),
+        centerTitle: true,
+        elevation: 0.0,
+      ),
+      body: MySnackBar(),
+    );
+  }
+}
+
+class MySnackBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context2) {
+    return Center(
+      child: OutlinedButton(
+        child: Text('Press me'),
+        onPressed: () {
+          // Scaffold가 context2보다 상위에 있기 때문에 이제 context가 Scaffold를 찾을 수 있게 된다.
+          ScaffoldMessenger.of(context2).showSnackBar(SnackBar(
+            content: Text('Hellow'),
+          ));
+        },
+      ),
+    );
+  }
+}
