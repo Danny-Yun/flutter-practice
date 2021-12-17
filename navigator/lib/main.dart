e5622640fb82a8e2a4406cc761b795a9670f7d91
Riudiu <lacvert13@gmail.com>
2021-12-17 10:45:33 +0900

    push 함수 형태 추가

--- navigator/lib/main.dart
+++ navigator/lib/main.dart
@@ -29,12 +29,15 @@ class FirstPage extends StatelessWidget {
         child: ElevatedButton(
             child: Text('Go to the Second page'),
             onPressed: () {
+              // push 함수에서 입력 값인 두개의 값 중 하나인 context는 현재 페이지의 위치정보를 확인, Route는 이동할 페이지 정보
               Navigator.push(
-                context,
-                MaterialPageRoute(builder: (BuildContext context) {
-                  return SecondPage();
-                }),
-              );
+                  context, MaterialPageRoute(builder: (_) => SecondPage()));
+              // Navigator.push(
+              //   context,
+              //   MaterialPageRoute(builder: (BuildContext context) {
+              //     return SecondPage();
+              //   }),
+              // );
             }),
       ),
     );
